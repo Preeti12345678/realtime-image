af9d6cf8d4aa34badadd93cd0650845e86c4bf25
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-06-01 19:40:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Tz_cQbISU/model.json", modelLoaded);
+    
+}
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video,gotresults);
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+}
